6e3c7e7248c346b768a8a6aad40bf782117fa50c
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-01 11:26:59 -0600

    adding app.js to gitrepo hopefully

---
+++ app.js
@@ -0,0 +1,28 @@
+var theBand = {
+    members: [{
+        name: 'Johnny P',
+        instrument: 'Sax'
+    },{
+        name: 'River',
+        instrument: 'Drums'
+    },{
+        name: 'Kris',
+        instrument: 'Guitar'
+    }],
+}
+
+// array = [1,2,3,4,5,6,7]
+// indicies 0 1 2 3 4 5 6 
+
+
+//Write a function that accepts a name argument and will loop over the members in the band members and return the band member and the instrument that he/she plays
+function findMemberByName(name){
+  //WHAT DO YOU WANT TO DO WITH THE MEMBER IF they were found
+}
+
+
+
+//Write a function that accepts an instrument argument and will loop over the members in the band members and return the band member who plays the requested instrument
+function findMemberByInstrument(instrument){
+
+}
